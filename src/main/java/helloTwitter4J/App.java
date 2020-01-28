3c41a9f652c84d20b209c6ed5104128062d6734f
thufir <hawat.thufir@gmail.com>
2020-01-28 02:17:11 -0800

    incremental

--- src/main/java/helloTwitter4J/App.java
+++ src/main/java/helloTwitter4J/App.java
@@ -3,7 +3,9 @@ package helloTwitter4J;
 import java.io.IOException;
 import java.util.InvalidPropertiesFormatException;
 import java.util.Properties;
+import java.util.Set;
 import java.util.logging.Logger;
+import twitter4j.Twitter;
 
 public class App {
 
@@ -13,11 +15,37 @@ public class App {
     private final Properties properties = new Properties();
 
 
-	private void loadProperties() {
+	private void loadProperties() throws InvalidPropertiesFormatException, IOException {
+
+        properties.loadFromXML(App.class.getResourceAsStream("/twitter.xml"));
+        log.info(properties.toString());
+
+        Set<Object> keySet = properties.keySet();
+        String key = null;
+        String value = null;
+
+        for (Object obj : keySet) {
+            key = obj.toString();
+            value = System.getenv(key);
+            log.info(key+value);
+        }
+        foo();
 	}
 
 
-    public static void main(String[] args)              throws InvalidPropertiesFormatException, IOException {
+    private void foo() {
+        Twitter twitter;
+      //  twitter = TwitterFactory.getSingleton;
+        // Twitter twitter = TwitterFactory.getSingleton();
+        // Query query = new Query("source:twitter4j yusukey");
+        // QueryResult result = twitter.search(query);
+        // for (Status status : result.getTweets()) {
+        // log.info("@" + status.getUser().getScreenName() + ":" + status.getText());
+        // }
+
+    }
+
+    public static void main(String[] args) throws InvalidPropertiesFormatException, IOException {
         new App().loadProperties();
     }
 
