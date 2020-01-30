d83e26f826ebcb19a54bda2129d98c4695a84d05
thufir <hawat.thufir@gmail.com>
2020-01-29 21:56:36 -0800

    incremental

--- src/main/java/helloTwitter4J/App.java
+++ src/main/java/helloTwitter4J/App.java
@@ -5,10 +5,8 @@ import java.util.InvalidPropertiesFormatException;
 import java.util.Properties;
 import java.util.Set;
 import java.util.logging.Logger;
-
 import twitter4j.Query;
 import twitter4j.QueryResult;
-//import twitter4j.Status;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
@@ -17,14 +15,14 @@ import twitter4j.conf.ConfigurationBuilder;
 public class App {
 
     private static final Logger log = Logger.getLogger(App.class.getName());
-    private final Properties twitterProperties = new Properties();
+    //private final Properties properties = new Properties();
 
-    private void loadProperties() throws InvalidPropertiesFormatException, IOException {
+    private Properties loadProperties() throws InvalidPropertiesFormatException, IOException {
+        Properties properties = new Properties();
+        properties.loadFromXML(App.class.getResourceAsStream("/twitter.xml"));
+        log.fine(properties.toString());
 
-        twitterProperties.loadFromXML(App.class.getResourceAsStream("/twitterProperties.xml"));
-        log.fine(twitterProperties.toString());
-
-        Set<Object> keySet = twitterProperties.keySet();
+        Set<Object> keySet = properties.keySet();
         String key = null;
         String value = null;
 
@@ -32,29 +30,28 @@ public class App {
             key = obj.toString();
             value = System.getenv(key);
             log.fine(key + value);
-            twitterProperties.setProperty(key, value);
+            properties.setProperty(key, value);
         }
+        return properties;
     }
 
     private void runQuery() throws TwitterException, InvalidPropertiesFormatException, IOException {
-        loadProperties();
-        log.fine(twitterProperties.toString());  //this matches what powershell uses
+        properties = loadProperties();
+        log.fine(properties.toString());  //this matches what powershell uses
         ConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
         configurationBuilder.setDebugEnabled(true)
-                .setOAuthConsumerKey(twitterProperties.getProperty("oAuthConsumerKey"))
-                .setOAuthConsumerSecret(twitterProperties.getProperty("oAuthConsumerSecret"))
-                .setOAuthAccessToken(twitterProperties.getProperty("oAuthAccessToken"))
-                .setOAuthAccessTokenSecret(twitterProperties.getProperty("oAuthAccessTokenSecret"));
+                .setOAuthConsumerKey(properties.getProperty("oAuthConsumerKey"))
+                .setOAuthConsumerSecret(properties.getProperty("oAuthConsumerSecret"))
+                .setOAuthAccessToken(properties.getProperty("oAuthAccessToken"))
+                .setOAuthAccessTokenSecret(properties.getProperty("oAuthAccessTokenSecret"));
 
         Twitter twitter = new TwitterFactory(configurationBuilder.build()).getSingleton();
-
-       
 
         // Twitter twitter = new TwitterFactory(configurationBuilder.build()).getSingleton();
         //   twitter. //       TwitterFactory twitterFactory = new TwitterFactory(configurationBuilder.build);
         //   Twitter twitter = TwitterFactory.getSingleton();
         Query query = new Query("source:twitter4j yusukey");
-         QueryResult result = twitter.search(query);
+        QueryResult result = twitter.search(query);
         //   for (Status status : result.getTweets()) {
         //       log.info("@" + status.getUser().getScreenName() + ":" + status.getText());
         //    }
